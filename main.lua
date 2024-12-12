c8d7d6f114c1f18217b57e6dc78c65025be17fd9
tistyse <112744097+tistyse@users.noreply.github.com>
2024-12-12 23:20:56 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -53,7 +53,7 @@ local Toggle = Tab:CreateToggle({
     CurrentValue = false,
     Flag = "FB", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
     Callback = function(Value)
-        if Toggle.CurrentValue == false then 
+        if Value == false then 
             print("false")
             L.Ambient = (Color3.fromRGB(DF.A))
             L.Brightness = DF.B
@@ -70,7 +70,7 @@ local Toggle = Tab:CreateToggle({
             L.FogEnd = DF.FE
             L.FogStart = DF.FS
         end
-        if Toggle.CurrentValue == true then
+        if Value == true then
             print("true")
             L.Ambient = (Color3.fromRGB(70, 70, 70))
             L.Brightness = 3
