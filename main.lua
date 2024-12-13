8d16b5420ca649826edffc600c0f67065a145341
tistyse <112744097+tistyse@users.noreply.github.com>
2024-12-13 18:57:05 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/SiriusSoftwareLtd/Rayfield/main/source.lua'))()
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local L = game.Lighting
 DF = {
     A = game.Lighting.Ambient,
